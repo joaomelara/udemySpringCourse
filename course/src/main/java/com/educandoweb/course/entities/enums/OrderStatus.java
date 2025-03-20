eea5639767c2968ee73225fbffd3dcd1eb6dfff4
joaomelara <joaopedromelara@gmail.com>
2025-03-19 23:54:41 -0300

    new project and some initial configurations

---
+++ course/src/main/java/com/educandoweb/course/entities/enums/OrderStatus.java
@@ -0,0 +1,21 @@
+package com.educandoweb.course.entities.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT(1),
+    PAID(2),
+    SHIPPED(3),
+    DELIVERED(4),
+    CANCELED(5);
+    
+    
+
+    private int code;
+    
+    private OrderStatus(int i) {
+	this.code = code;
+    }
+    
+    public int getCode() {
+	return code;
+    }
+}
